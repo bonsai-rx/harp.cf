12e3e06f516fcfe1ac5c5521adc467c6052acaab
glopes <glopes@localhost>
2017-11-20 22:30:54 +0000

    Added commands and events for specific Harp devices.

---
+++ Bonsai.Harp.CF/SynchronizerCommand.cs
@@ -0,0 +1,56 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.ComponentModel;
+
+namespace Bonsai.Harp.CF
+{
+    public enum SynchronizerCommandType : byte
+    {
+        Outputs
+    }
+
+    [Description(
+        "\n" +
+        "Outputs: Bitmask\n"
+    )]
+
+    public class SynchronizerCommand : SelectBuilder, INamedElement
+    {
+        public SynchronizerCommand()
+        {
+            Type = SynchronizerCommandType.Outputs;
+        }
+
+        string INamedElement.Name
+        {
+            get { return typeof(SynchronizerCommand).Name + "." + Type.ToString(); }
+        }
+
+        public SynchronizerCommandType Type { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
+        {
+            switch (Type)
+            {
+                case SynchronizerCommandType.Outputs:
+                    if (expression.Type != typeof(byte)) { expression = Expression.Convert(expression, typeof(byte)); }
+                    return Expression.Call(typeof(SynchronizerCommand), "ProcessOutputs", null, expression);
+
+                default:
+                    break;
+            }
+            return expression;
+        }
+
+        static HarpDataFrame ProcessOutputs(byte input)
+        {
+            return HarpDataFrame.UpdateChesksum(new HarpDataFrame(2, 5, 33, 255, (byte)HarpType.U8, input, 0));
+        }
+    }
+}
