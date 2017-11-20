2afa9110018a58e739da9b2b2d01805499892fc0
glopes <glopesdev@users.noreply.github.com>
2017-11-20 22:30:54 +0000

    Added commands and events for specific Harp devices.

---
+++ Bonsai.Harp.CF/PyControlCommand.cs
@@ -0,0 +1,63 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.ComponentModel;
+
+namespace Bonsai.Harp.CF
+{
+    public enum PyControlCommandType : byte
+    {
+        UseBnc2,
+        UseDio6B
+    }
+
+    [Description(
+        "\n" +
+        "UseBnc1: Any\n" +
+        "UseDio6B: Any\n"
+    )]
+    public class PyControlCommand : SelectBuilder, INamedElement
+    {
+        public PyControlCommand()
+        {
+            Type = PyControlCommandType.UseBnc2;
+        }
+
+        string INamedElement.Name
+        {
+            get { return typeof(PyControlCommand).Name + "." + Type.ToString(); }
+        }
+
+        public PyControlCommandType Type { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
+        {
+            switch (Type)
+            {
+                case PyControlCommandType.UseBnc2:
+                    return Expression.Call(typeof(PyControlCommand), "ProcessUseBnc1", new[] { expression.Type }, expression);
+                case PyControlCommandType.UseDio6B:
+                    return Expression.Call(typeof(PyControlCommand), "ProcessUseDio6B", new[] { expression.Type }, expression);
+
+                default:
+                    break;
+            }
+            return expression;
+        }
+
+        static HarpDataFrame ProcessUseBnc2<TSource>(TSource input)
+        {
+            return HarpDataFrame.UpdateChesksum(new HarpDataFrame(2, 5, 42, 255, (byte)HarpType.U8, 1, 0));
+        }
+
+        static HarpDataFrame ProcessUseDio6B<TSource>(TSource input)
+        {
+            return HarpDataFrame.UpdateChesksum(new HarpDataFrame(2, 5, 42, 255, (byte)HarpType.U8, 0, 0));
+        }
+    }
+}
