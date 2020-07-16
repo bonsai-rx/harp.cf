deed4412c00d9d6e1e2b71aa51f47c4a7e3d66cf
glopesdev <glopesdev@users.noreply.github.com>
2020-07-16 23:22:42 +0100

    Update project to the new csproj format

--- Bonsai.Harp.CF/Properties/AssemblyInfo.cs
+++ Bonsai.Harp.CF/Properties/AssemblyInfo.cs
@@ -1,37 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Bonsai;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai - Harp CF Library")]
-[assembly: AssemblyDescription("Bonsai Library for data acquisition and control of Champalimaud Foundation devices implementing the Harp protocol.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Filipe Carvalho, Gonçalo Lopes")]
-[assembly: AssemblyProduct("Bonsai.Harp.CF")]
-[assembly: AssemblyCopyright("Copyright © Filipe Carvalho and Gonçalo Lopes 2017-2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("16b132b4-7ae6-43b1-9dbb-9b6dfc93df8d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
-[assembly: AssemblyInformationalVersion("1.6.0")]
+[assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Harp.CF", "cf")]
+[assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Daq")]
