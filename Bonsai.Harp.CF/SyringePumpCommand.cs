f8cb049f41bde739cb5e7b0e8709b3f8f6b18bb0
Luís Teixeira <micboucinha@gmail.com>
2020-10-09 14:14:55 +0100

    Add Set and Clear Digital Outputs commands

--- Bonsai.Harp.CF/SyringePumpCommand.cs
+++ Bonsai.Harp.CF/SyringePumpCommand.cs
@@ -12,6 +12,9 @@ namespace Bonsai.Harp.CF
 
         StartProtocol,
         StopProtocol,
+
+        SetDigitalOutputs,
+        ClearDigitalOutputs,
     }
 
     [TypeDescriptionProvider(typeof(DeviceTypeDescriptionProvider<SyringePumpCommand>))]
@@ -34,6 +37,8 @@ namespace Bonsai.Harp.CF
                     case SyringePumpCommandType.DisableMotorDriver: return "Disables the motor on the Syringe Pump";
                     case SyringePumpCommandType.StartProtocol: return "Start the configured protocol on the Syringe Pump";
                     case SyringePumpCommandType.StopProtocol: return "Stop the running protocol on the Syringe Pump";
+                    case SyringePumpCommandType.SetDigitalOutputs: return "Set the state of digital output 0 using the input boolean value.";
+                    case SyringePumpCommandType.ClearDigitalOutputs: return "Set the state of digital output 1 using the input boolean value.";
 
                     default: return null;
                 }
@@ -52,6 +57,12 @@ namespace Bonsai.Harp.CF
                     return Expression.Call(typeof(SyringePumpCommand), nameof(ProcessStartProtocol), null);
                 case SyringePumpCommandType.StopProtocol:
                     return Expression.Call(typeof(SyringePumpCommand), nameof(ProcessStopProtocol), null);
+                case SyringePumpCommandType.SetDigitalOutputs:
+                    if(expression.Type != typeof(byte)) { expression = Expression.Convert(expression, typeof(byte)); }
+                    return Expression.Call(typeof(SyringePumpCommand), nameof(ProcessSetDigitalOutputs), null, expression);
+                case SyringePumpCommandType.ClearDigitalOutputs:
+                    if (expression.Type != typeof(byte)) { expression = Expression.Convert(expression, typeof(byte)); }
+                    return Expression.Call(typeof(SyringePumpCommand), nameof(ProcessSetDigitalOutputs), null, expression);
                 default:
                     throw new InvalidOperationException("Invalid selection or not supported yet.");
             }
@@ -61,5 +72,7 @@ namespace Bonsai.Harp.CF
         static HarpMessage ProcessDisableMotorDriver() => HarpCommand.WriteByte(address: 32, 0);
         static HarpMessage ProcessStartProtocol() => HarpCommand.WriteByte(address: 33, 1);
         static HarpMessage ProcessStopProtocol() => HarpCommand.WriteByte(address: 33, 0);
+        static HarpMessage ProcessSetDigitalOutputs(byte input) => HarpCommand.WriteByte(address: 39, input);
+        static HarpMessage ProcessClearDigitalOutputs(byte input) => HarpCommand.WriteByte(address: 40, input);
     }
 }
